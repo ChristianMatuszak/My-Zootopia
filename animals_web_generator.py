41e884da70dc1a34aef35105c59eaa74ea060433
ChristianMatuszak <c.matuszak@outlook.com>
2025-02-26 13:02:05 +0100

    - Added more fields and spaces between card_titel and card_text
    - Updated animals_template to get a bigger animal titel and centered it
    
    - Updated animals_web_generator to open html and json files with utf-8 encoder to display everything correctly
    
    - Updated animals_data.json a few fields because of bugged view in browser

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,32 +1,51 @@
 import json
 
-with open("animals_template.html", "r") as file:
+with open("animals_template.html", "r", encoding="utf-8") as file:
     template_content = file.read()
 
-
-with open("animals_data.json", "r") as file:
+with open("animals_data.json", "r", encoding="utf-8") as file:
     animals_data = json.load(file)
 
-animals_output = ""
-for animal in animals_data:
+
+def serialize_animal(animal):
+    """Serialize a single animal into an HTML list item with separate fields."""
     name = animal['name']
     diet = animal['characteristics']['diet']
     location = animal['locations'][0]
     type_ = animal['characteristics'].get('type')
+    lifespan = animal['characteristics'].get('lifespan')
+    color = animal['characteristics'].get('color')
+    scientific_name = animal['taxonomy'].get('scientific_name')
 
-    animals_output += '<li class="cards__item">'
-    animals_output += f'  <div class="card__title">{name}</div>'
-    animals_output += '  <p class="card__text">'
-    animals_output += f'      <strong>Diet:</strong> {diet}<br/>'
-    animals_output += f'      <strong>Location:</strong> {location}<br/>'
+    output = '<li class="cards__item">\n'
+    output += f'  <div class="card__title">{name}</div>\n'
+    output += '  <br/>\n'
+    output += '  <div class="card__text">\n'
+    output += '    <ul class="animal-details">\n'
+    output += f'      <li class="animal-detail"><strong>Diet:</strong> {diet}</li>\n'
+    output += f'      <li class="animal-detail"><strong>Location:</strong> {location}</li>\n'
     if type_:
-        animals_output += f'      <strong>Type:</strong> {type_}<br/>'
-    animals_output += '  </p>'
-    animals_output += '</li>\n'
+        output += f'      <li class="animal-detail"><strong>Type:</strong> {type_}</li>\n'
+    if lifespan:
+        output += f'      <li class="animal-detail"><strong>Lifespan:</strong> {lifespan}</li>\n'
+    if color:
+        output += f'      <li class="animal-detail"><strong>Color:</strong> {color}</li>\n'
+    if scientific_name:
+        output += f'      <li class="animal-detail"><strong>Scientific Name:</strong> {scientific_name}</li>\n'
+    output += '    </ul>\n'
+    output += '  </div>\n'
+    output += '</li>\n'
+
+    return output
+
+
+animals_output = ""
+for animal in animals_data:
+    animals_output += serialize_animal(animal)
 
 updated_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", animals_output)
 
-with open("animals.html", "w") as file:
+with open("animals.html", "w", encoding="utf-8") as file:
     file.write(updated_html_content)
 
 print("animals.html has been created successfully.")
